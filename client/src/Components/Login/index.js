4ab93f4fd5856bbe62ad4ca574d9efff5ddc21d1
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-01 20:35:06 -0400

    components

---
+++ client/src/Components/Login/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div>
+                <h3>STOCKBot Login</h3>
+            </div>
+        );
+    }
+}
---
+++ client/src/Components/SignUp/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class SignUp extends Component {
+    render() {
+        return (
+            <div>
+                <h3>StOCKBOT SignUp</h3>
+            </div>
+        );
+    }
+}
