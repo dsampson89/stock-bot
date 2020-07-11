3eab7586e89fab25b77b7e34e191987d03b9c28d
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-11 09:00:05 -0400

    header form

--- client/src/Components/Header/header.js
+++ client/src/Components/Header/header.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
 import Bot from '../../API/alpaca'
+import style from "./style.css"
 export class header extends Component {
     constructor(props) {
         super(props);
         this.state = {
             bot: new Bot('paper', 'PKV7RSE5YZS4KCV3RTYD', '8Yt2e5xM3LQwq0C2KDXnHUlhNllgEbQjhBLlj5Dd')
-        }
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value})
+    }
+
+    handleSubmit(event){
+        alert("Search submited for: " + this.state.value);
+        event.preventDefault();
     }
 
     render() {
@@ -16,6 +30,10 @@ export class header extends Component {
                 <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id metus sed diam tristique tristique.</p>
                 <hr className="my-4"/>
                 <p>Build onto you portfolio</p>
+                <form className="input" onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+                <input type="submit" value="Submit" />
+                </form>
                 <a className="btn btn-primary btn-lg" href="/stocks" role="button">Buy Stocks</a>
             </div>
         )
