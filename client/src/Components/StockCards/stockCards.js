e996a84b7757eb08a5bf7be8b728660482e846a3
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-12 17:42:43 -0400

    chart

--- client/src/Components/StockCards/stockCards.js
+++ client/src/Components/StockCards/stockCards.js
@@ -3,56 +3,26 @@ import Bot from '../../API/alpaca'
 
 class stockCards extends Component {
 
-   dataGrab = async () => {
-    var bot = new Bot('paper', 'PKV7RSE5YZS4KCV3RTYD', '8Yt2e5xM3LQwq0C2KDXnHUlhNllgEbQjhBLlj5Dd')
-     bot.getStats(this.state.value).then((response)=>{
-       console.log(response)
-     })
-   }
+   
 
- handleChange(event) {
-    this.setState({value: event.target.value})
- }
 
-handleSubmit(event){
-    alert("Search submited for: " + this.state.value);
-    event.preventDefault();
-    
-}
   render() {
 
-    const cardInfo = [
-      {image:"", title:"", text:""}
-    ];
-
-    const renderCard = (card, index) => {
       return(
         <div>
-          <form className="input" onSubmit={this.handleSubmit}>
+          
                     <div className="form-row">
                         <div className="col">
-                            <input type="text" className="form-control" value={this.state} onChange={this.handleChange}/>
+                            <input type="text" className="form-control" />
                         </div>
                         </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-            </form>
-        <div className="row">
-          <div className="col-md-3">
-            <div className="card" key={index}>
-              <img src={card.image} className="card-img-top" alt="..."/>
-              <div className="card-body">
-                  <h5 className="card-title">{card.title}</h5>
-                  <p className="card-text">{card.text}</p>
-                  <a href="/login" className="btn btn-success">Show Stock</a>
-              </div>
-            </div>
-          </div>
-        </div>
+                        <button className="btn btn-primary btn-lg" onClick={this.props.dataGrab}>Check Balance</button>
+            
+        
       </div>
       )
     }
-    return <div className="App">{cardInfo.map(renderCard)}</div>
   
   }
-}
+
 export default stockCards
--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,17 +1,43 @@
 import React from "react";
 import Stockcards from "../Components/StockCards/stockCards";
 import Header from "../Components/Header/header"
-import Results from "../Components/Results/results"
+import Charts from "../Components/Charts/charts"
 import Bot from "../API/alpaca"
 
 class Portfolio extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            output: [],
+        }
+    }
+    
+    dataGrab = async () => {
+        var bot = new Bot('paper', 'PKV7RSE5YZS4KCV3RTYD', '8Yt2e5xM3LQwq0C2KDXnHUlhNllgEbQjhBLlj5Dd')
+         bot.getStats("AAPL").then((response)=>{
+            var prices = []
+            var objKey= Object.keys(response)
+            var whatever = (response[objKey])
+            console.log(whatever)
+            for(var i = 0; i < whatever.length; i ++){
+                prices.push(whatever[i].closePrice)
+                console.log(whatever[i].closePrice)
+            }
+            this.setState((state)=>{
+                return {output: prices}
+            })
+        })
+    }
+
+
     render() {
         return (
             <div className="containter">
             <div>
                 <Header />
-                <Results />
-                <Stockcards />
+                <Charts output={this.state.output}/>
+                <Stockcards dataGrab={this.dataGrab} />
             </div>
             </div>
         )
