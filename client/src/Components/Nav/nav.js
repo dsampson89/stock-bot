f02667d6a050d9c5a40edb9cd73f9b21fc7a117b
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-18 11:46:14 -0400

    fontawesome

--- client/src/Components/Nav/nav.js
+++ client/src/Components/Nav/nav.js
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
 import React, { Component } from 'react'
 
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 class nav extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg">
       <div className="container" id="navbg">
-        <Link className="navbar-brand" to={"/sign-in"}>Stock<i class="fas fa-robot"></i>BOT</Link>
+        <Link className="navbar-brand" to={"/sign-in"}>StockB<FontAwesomeIcon icon={faRobot} />T</Link>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item">
