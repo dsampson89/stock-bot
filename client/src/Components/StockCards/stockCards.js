5198ec78c76ee688030bc873563632c839587ad1
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-18 09:53:52 -0400

    css changes

--- client/src/Components/StockCards/stockCards.js
+++ client/src/Components/StockCards/stockCards.js
@@ -6,7 +6,7 @@ class stockCards extends Component {
       <div className="container">  
         <div className="row">
           <div className="col-lg-12">
-              <input type="text" placeholder="Stock Symbol" className="form-control" />
+              <input type="text" placeholder="Stock Symbol" className="form-control" id="symbolInput" />
               <button className="btn btn-primary btn-lg" id="symbolButton" onClick={this.props.dataGrab}>Check Prices</button>
           </div>
         </div>
