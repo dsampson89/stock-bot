2ef878ac2a93c0b8e32da667f7daac49d156bef1
Dexter Sampson <dexter1.sampson@gmail.com>
2020-07-20 18:05:25 -0400

    carosel

--- client/src/Components/SignUp/index.js
+++ client/src/Components/SignUp/index.js
@@ -4,25 +4,51 @@ const Signup = () => {
 
         return (
             <div className="container" id="signUp">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <h3>61% of daily stock trades are high frequency trading bots.</h3>
-                        <p>The biggest names in stocks are all using algorithmic trading. Why shouldn't you?</p>
-                        <p>Only 10% of trades are done manually, and those traders are losing out to Wall Street firms with computers that
-                            can make trades faster than you can click "buy"
-                        </p>
-                        <h2>We wanted to even the playing field.</h2>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-lg-12">
-                        <h3>Our bot is easy to use</h3>
-                        <ol>
-                            <li>Sign up for a trading account with alpaca.markets</li>
-                            <li>Enter your api information <a href="/" role="button">here</a></li>
-                            <li>Click go and watch the profits come in, like a true Wolf of Wall Street</li>
-                        </ol>
-                    </div>
+                <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div className="carousel-inner">
+                        <div className="carousel-item active">
+                            <img src="https://specials-images.forbesimg.com/imageserve/1201457952/960x0.jpg?fit=scale" className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>61% of daily stock trades are high frequency trading bots.</h1>
+                                <p>The biggest names in stocks are all using algorithmic trading. Why shouldn't you?</p>
+                                <p>Only 10% of trades are done manually, and those traders are losing out to Wall Street firms with computers that
+                                    can make trades faster than you can click "buy"
+                                </p>
+
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://specials-images.forbesimg.com/imageserve/1201457952/960x0.jpg?fit=scale" className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>We wanted to even the playing field.</h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
+                        </div>
+                        <div className="carousel-item">
+                            <img src="https://specials-images.forbesimg.com/imageserve/1201457952/960x0.jpg?fit=scale" className="d-block w-100" alt="..."/>
+                            <div className="carousel-caption d-none d-md-block">
+                                <h1>Our bot is easy to use</h1>
+                                <ol>
+                                    <li>Sign up for a trading account with alpaca.markets</li>
+                                    <li>Enter your api information <a href="/" role="button">here</a></li>
+                                    <li>Click go and watch the profits come in, like a true Wolf of Wall Street</li>
+                                </ol>
+                            </div>
+                        </div>
+                     </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
                 </div>
             </div>
         );
